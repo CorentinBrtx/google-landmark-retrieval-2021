5996b2ecf19d8fd2734bad3d65a31683e80f97e0
Corentin Berteaux <corentin.berteaux98@gmail.com>
2022-01-27 10:21:11 +0100

    Change training script

--- slurm_scripts/run_training.sh
+++ slurm_scripts/run_training.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=4
 #SBATCH --mail-user=corentin.berteaux@student-cs.fr
@@ -19,4 +19,4 @@ module load cuda/10.2.89/intel-19.0.3.199
 source activate landmark-retrieval
 
 # Run training
-python ~/google-landmark-retrieval-2021/src/scripts/train.py -d $WORKDIR/landmark-retrieval -f 512 -n 1000 -b 32 -lr 1e-3 --image_size 224 --log_interval 10 --num_workers 4
+python ~/google-landmark-retrieval-2021/src/scripts/train.py b4_m0_380 -d $WORKDIR/landmark-retrieval --resume-training --efficientnet efficientnet-b4 --feature-size 512 --nb-epochs 500 --batch-size 32 -lr 1e-3 -m 1 --image-size 380 --log-interval 20 --num-workers 8
